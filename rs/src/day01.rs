62f3c466a4b0f523a266b6e0faa7a397eb03f71a
Sebastiano Rebonato-Scott <141144328+all-c-a-p-s@users.noreply.github.com>
2024-12-01 11:24:46 +0100

    day 1 in rs

---
+++ rs/src/day01.rs
@@ -0,0 +1,42 @@
+const INPUT: &str = "./input01.txt";
+
+fn count_appearances(t: i32, v: &[i32]) -> i32 {
+    v.iter().fold(0, |acc, x| acc + if *x == t { 1 } else { 0 })
+}
+
+fn get_lists() -> (Vec<i32>, Vec<i32>) {
+    let f = std::fs::read_to_string(INPUT).unwrap();
+    let lines: Vec<String> = f.lines().map(|l| l.to_string()).collect();
+
+    let (mut first_list, mut second_list) = (Vec::new(), Vec::new());
+
+    for line in lines {
+        let nums: Vec<i32> = line
+            .split_ascii_whitespace()
+            .map(|x| x.parse::<i32>().unwrap())
+            .collect();
+        first_list.push(nums[0]);
+        second_list.push(nums[1]);
+    }
+    (first_list, second_list)
+}
+
+pub fn part_one() -> i32 {
+    let (mut first_list, mut second_list) = get_lists();
+
+    first_list.sort();
+    second_list.sort();
+
+    first_list
+        .iter()
+        .enumerate()
+        .fold(0, |acc, (i, x)| acc + (second_list[i] - x).abs())
+}
+
+pub fn part_two() -> i32 {
+    let (first_list, second_list) = get_lists();
+
+    first_list
+        .iter()
+        .fold(0, |acc, x| acc + count_appearances(*x, &second_list) * x)
+}
---
+++ rs/src/main.rs
@@ -0,0 +1,33 @@
+use std::io::{stdout, Write};
+use std::time::Instant;
+
+mod day01;
+
+fn take_int() -> i32 {
+    let mut input = String::new();
+    std::io::stdin().read_line(&mut input).unwrap();
+    input.trim().parse().unwrap()
+}
+
+fn main() {
+    print!("Enter day to solve: ");
+    let _ = stdout().flush();
+    let day = take_int();
+
+    print!("Enter part to solve: ");
+    let _ = stdout().flush();
+    let part = take_int();
+
+    println!();
+    let start = Instant::now();
+
+    print!("Answer: ");
+    match (day, part) {
+        (1, 1) => println!("{}", day01::part_one()),
+        (1, 2) => println!("{}", day01::part_two()),
+        _ => panic!("sorry not implemented yet"),
+    };
+
+    let duration = start.elapsed();
+    println!("Runtime: {:?}", duration);
+}
